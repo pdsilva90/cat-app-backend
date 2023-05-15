733e431c6a6bd41547e48078d058ad2b44be4ec3
pdsilva90 <pdsilva90@gmail.com>
2023-05-15 15:17:30 -0500

    full CRUD routes

---
+++ routes/cats.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+//ROUTES
+//GET all cats
+router.get('/cats', (req,res) => {
+    res.send('This is the GET ROUTE for all cats!');
+});
+
+//POST a new cat
+router.post('/cats', (req,res) => {
+    res.send('This is the POST route for a new cat!');
+});
+
+//GET a single cat
+router.get('/cats/:id', (req, res) => {
+    res.send(`This is the GET route for a single cat found at ${ req.params.id }`);
+});
+
+//PUT to update  a cat
+router.put('/cats/:id', (req, res) => {
+    res.send(`This is the PUT route for a cat: ${ req.params.id }`)
+});
+
+//DELETE to destroy a cat
+router.delete('cats/:id', (req, res) => {
+    res.send(`This is the Delete route for a cat: Bye bye ${ req.params.id}`)
+});
+
+router.listen(port, () => {
+    console.log('I am listening at port 3000');
+});
+
+module.export = router
--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 const express = require('Express');
 const app = express();
 const port = 3000;
+const catRoutes = require('./routes/cats')
 
 //ROUTES
 //GET all cats
-app.get('/cats', (req,res) => {
+app.get('/', (req,res) => {
     res.send('This is the GET ROUTE for all cats!');
 });
 
 //POST a new cat
-app.post('/cats', (req,res) => {
+app.post('/', (req,res) => {
     res.send('This is the POST route for a new cat!');
 });
 
 //GET a single cat
-app.get('/cats/:id', (req, res) => {
+app.get('/:id', (req, res) => {
     res.send(`This is the GET route for a single cat found at ${ req.params.id }`);
 });
 
 //PUT to update  a cat
-app.put('/cats/:id', (req, res) => {
+app.put('/:id', (req, res) => {
     res.send(`This is the PUT route for a cat: ${ req.params.id }`)
 });
 
 //DELETE to destroy a cat
-app.delete('cats/:id', (req, res) => {
+app.delete('/:id', (req, res) => {
     res.send(`This is the Delete route for a cat: Bye bye ${ req.params.id}`)
 });
 

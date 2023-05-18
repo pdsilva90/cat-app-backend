acf4ac65a4639f09b2d9161546377556ab736385
pdsilva90 <pdsilva90@gmail.com>
2023-05-18 15:09:27 -0500

    add cors

--- routes/cats.js
+++ routes/cats.js
@@ -1,32 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+const Cat = require('../models/cat');
+
+
+async function main() {
+  await mongoose.connect('mongodb+srv://pdsilva90:Pdsilva12!@cluster0.erqhdng.mongodb.net/catAppDB?retryWrites=true&w=majority');
+console.log('Connected to MongoDB');
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+ main().catch(err => console.log(err));
+
 
 //ROUTES
 //GET all cats
-router.get('/cats', (req,res) => {
-    res.send('This is the GET ROUTE for all cats!');
+router.get('/', async (req,res) => {
+    try {
+        const cats = await Cat.find({});
+        res.json(cats)
+    } catch {
+        res.json({ msg: 'There was a problem getting all of the cats!'});
+    }
 });
 
-//helloo 
-
 //POST a new cat
-router.post('/cats', (req,res) => {
-    res.send('This is the POST route for a new cat!');
+router.post('/', async (req,res) => {
+    try {
+    const cat = await Cat.create(req.body);
+    res.json(cat);
+    } catch {
+    res.json({ msg: 'There was a problem creating your cat.'});
+    }
 });
 
 //GET a single cat
-router.get('/cats/:id', (req, res) => {
-    res.send(`This is the GET route for a single cat found at ${ req.params.id }`);
+router.get('/:id', async (req, res) => {
+    try {
+        const cat = await Cat.findById(req.params.id);
+        res.json(cat)
+    } catch {
+        res.json({ msg: 'There was a problem getting your cat.'});
+    }
 });
 
 //PUT to update  a cat
-router.put('/cats/:id', (req, res) => {
-    res.send(`This is the PUT route for a cat: ${ req.params.id }`)
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedCat = await Cat.findByIdAndUpdate(req.params.id, { set: {name: req.body.name}}, { new: true })
+        //new returns the update object instead of the previous object 
+        res.json(updateCat);
+    } catch {
+        res.json({ msg: 'There was problem updating your cat.'})
+    }
 });
 
 //DELETE to destroy a cat
-router.delete('cats/:id', (req, res) => {
-    res.send(`This is the Delete route for a cat: Bye bye ${ req.params.id}`)
+router.delete('/:id', async (req, res) => {
+    try {
+        await Cat.findByIdAndDelete(req.params.id);
+        res.json({ msg: 'This cat has successfully been adopted'});
+    } catch {
+        res.json({ msg: 'There was a problem deleting your cat'})
+    }
 });
 
 
--- server.js
+++ server.js
@@ -1,33 +1,16 @@
 const express = require('Express');
 const app = express();
 const port = 3000;
-const catRoutes = require('./routes/cats')
+const cors = require('cors');
+const catRouter = require('./routes/cats')
 
-//ROUTES
-//GET all cats
-app.get('/', (req,res) => {
-    res.send('This is the GET ROUTE for all cats!');
-});
+//middleware
+app.use(express.urlencoded({ extended: true }));
 
-//POST a new cat
-app.post('/', (req,res) => {
-    res.send('This is the POST route for a new cat!');
-});
 
-//GET a single cat
-app.get('/:id', (req, res) => {
-    res.send(`This is the GET route for a single cat found at ${ req.params.id }`);
-});
-
-//PUT to update  a cat
-app.put('/:id', (req, res) => {
-    res.send(`This is the PUT route for a cat: ${ req.params.id }`)
-});
-
-//DELETE to destroy a cat
-app.delete('/:id', (req, res) => {
-    res.send(`This is the Delete route for a cat: Bye bye ${ req.params.id}`)
-});
+//routes
+app.use('/cats', catRoutes);
+app.use(cors());
 
 app.listen(port, () => {
     console.log('I am listening at port 3000');
